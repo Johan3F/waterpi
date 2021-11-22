4ec00f9b187eb734d7a35735a795aacf8aca87b3
Johan <jvandewallemuniz@gmail.com>
2021-11-22 08:32:57 +0100

    Removing second reading

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ use mcp3008::Mcp3008;
 fn main() {
     if let Ok(mut mcp3008) = Mcp3008::new("/dev/spidev0.0") {
         loop {
-            println!(
-                "Channel 1 `{:?}` | Channel 7 `{:?}`",
-                mcp3008.read_adc(1),
-                mcp3008.read_adc(7)
-            );
+            println!("Channel 7 `{:?}`", mcp3008.read_adc(7));
             thread::sleep(Duration::from_secs(1))
         }
     }
