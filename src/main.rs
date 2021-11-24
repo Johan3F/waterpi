c866d601ee9feb943ee01cacf902ad4bd1d9ce4f
Johan <jvandewallemuniz@gmail.com>
2021-11-24 08:33:35 +0100

    reducing complexity a bit

--- src/main.rs
+++ src/main.rs
@@ -30,27 +30,30 @@ fn main() -> Result<(), Error> {
         Mcp3008::new("/dev/spidev0.0").expect("Unable to establish connection with MCP3008");
     let pump = WaterPump::new(PUMP_PIN).expect("Unable to initialize water pump");
 
-    let sensor = start_reading(
-        mcp3008,
-        quit_receiver.clone(),
-        SENSOR_POLLING_TIME,
-        READ_CHANNEL,
-    );
+    let sensor = start_reading(mcp3008, SENSOR_POLLING_TIME, READ_CHANNEL);
 
     loop {
-        let received = sensor.recv();
-        match received {
-            Ok(value) => {
-                println!("Sensor readings: {}", value);
-                if value > WATERING_THRESHOLD {
-                    let _ = pump.on();
-                } else {
-                    let _ = pump.off();
+        select! {
+            recv(sensor) -> received =>{
+                match received {
+                    Ok(value) => {
+                        println!("Sensor readings: {}", value);
+                        if value > WATERING_THRESHOLD {
+                            let _ = pump.on();
+                        } else {
+                            let _ = pump.off();
+                        }
+                    }
+                    Err(_) => {
+                        let _ = pump.stop();
+                        break;
+                    }
                 }
-            }
-            Err(_) => {
+            },
+            recv(quit_receiver) -> _ => {
+                println!("\nStopping system...");
                 let _ = pump.stop();
-                let _ = quit_sender.send(());
+                println!("\t- Pump stopped");
                 break;
             }
         }
@@ -59,17 +62,12 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn start_reading(
-    mut mcp3008: Mcp3008,
-    quit_channel: Receiver<()>,
-    polling_time: Duration,
-    channel: u8,
-) -> Receiver<u16> {
-    let (internal_sender, internal_receiver) = unbounded();
+fn start_reading(mut mcp3008: Mcp3008, polling_time: Duration, channel: u8) -> Receiver<u16> {
+    let (sender, receiver) = unbounded();
     std::thread::spawn(move || loop {
         match mcp3008.read_adc(channel) {
             Ok(read_value) => {
-                if let Err(_) = internal_sender.send(read_value) {
+                if let Err(_) = sender.send(read_value) {
                     println!("Unable to get value this iteration. Waiting for next");
                 }
             }
@@ -80,25 +78,5 @@ fn start_reading(
         thread::sleep(polling_time);
     });
 
-    let (outside_sender, outside_receiver) = unbounded();
-    std::thread::spawn(move || loop {
-        select! {
-            recv(internal_receiver) -> received => {
-                match received {
-                    Ok(value) => {
-                        if let Err(_) = outside_sender.send(value) {
-                            break;
-                        }
-                    }
-                    Err(_) => break,
-                };
-            },
-            recv(quit_channel) -> _ => {
-                println!("\nEnding sensor polling");
-                break;
-            },
-        }
-    });
-
-    outside_receiver
+    receiver
 }
